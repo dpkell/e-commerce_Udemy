2714c425fd82c76146ed5dc12677ee8bbaa60b60
Dpkell <donald.kell92@gmail.com>
2020-07-14 13:45:50 -0400

    Added Header component with logo and stylings

--- src/App.js
+++ src/App.js
@@ -5,23 +5,12 @@ import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
-
-
-{/** NOTES - REACT ROUTING CONCEPTS
-
-  Route: Renders components based upon path file, including any common path names used in other routes (think of it in terms of regex), 
-  so a common problem that can occur is rendering of sections intended to be separate "pages" due to common path names. Such as if the 
-  homepage's path is '/' and our hats section's path is '/hats'. Both would be rendered. This can be avoided by using either the exact 
-  property in Route or Switch.
-
-  Switch: Used to help manage routes and route paths. Switch is wrapped around Route and any matching Route path is the only Route that gets 
-  rendered. Useful to avoid rendering unnecessary components and thus help performance as well as keeping things clean and organized.
-
-*/}
+import Header from './components/header/header.component';
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />

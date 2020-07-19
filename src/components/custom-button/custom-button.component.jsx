857accd4c4853f854684e0229c145b0d5a404be6
Dpkell <donald.kell92@gmail.com>
2020-07-19 16:30:14 -0400

    Updated cart utils to group items in cart reducer

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -4,8 +4,17 @@ import './custom-button.styles.scss';
 
 // This component receives the children elements of the form and any other necessary properties (such as type) and acts upon it based upon
 // relative properties; such as type="submit" that is supplied via the ...otherProps spread operator.
-const CustomButton = ({children, isGoogleSignIn, ...otherProps }) => (
-    <button className={`${isGoogleSignIn ? 'google-sign-in' : ''} custom-button`} {...otherProps}>
+const CustomButton = ({
+    children, 
+    isGoogleSignIn,
+    inverted, 
+    ...otherProps }) => (
+    <button 
+        className={`${inverted ? 'inverted' : ''} ${
+            isGoogleSignIn ? 'google-sign-in' : ''
+        } custom-button`} 
+        {...otherProps}
+    >
         {children}
     </button>
 );

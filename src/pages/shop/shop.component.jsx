781e876b3252f57298ae175bc4a74ab234c23102
Dpkell <donald.kell92@gmail.com>
2020-07-20 11:13:26 -0400

    Added addItem and RemoveItem to checkout item component

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -14,7 +14,7 @@ class ShopPage extends React.Component {
         const {collections} = this.state;
         return (<div className="shop-page"> {
             collections.map(({ id, ...otherCollectionProps }) => (
-                <CollectionPreview key={collections.id} {...otherCollectionProps} />
+                <CollectionPreview key={id} {...otherCollectionProps} />
             ))
         }
 
